3e112ccc7dc9eabaa961ae5713787011e6c1b416
mahmoudnasser1561@gmail.com <mahmoudnasser1561@gmail.com>
2024-11-30 23:29:15 +0200

    resolved some bugs

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import PySimpleGUI as Sg
-from zip_creator import make_archive
+from compress.zip_creator import make_archive
 
 filesText = Sg.Text("Select files to compress:")
-filesInput = Sg.Input()
+filesInput = Sg.Input(key="files")  # Correct key for the file input
 fileChooseButton = Sg.FileBrowse("Choose Files", key="ChooseFiles")
 
 destinationText = Sg.Text("Select destination folder:")
-destinationInput = Sg.Input()
+destinationInput = Sg.Input(key="folder")
 destinationChooseButton = Sg.FolderBrowse()
 
 compress_button = Sg.Button("File Compressor")
+output = Sg.Text("", size=(40, 1), key="output")
 
-# Layout for the window
 layout = [
     [filesText, filesInput, fileChooseButton],
     [destinationText, destinationInput, destinationChooseButton],
-    [compress_button]
+    [compress_button],
+    [output]
 ]
 
 # Create the window
@@ -24,17 +25,19 @@ window = Sg.Window("File Compressor", layout=layout)
 # Event loop
 while True:
     event, values = window.read()
+
     if event == Sg.WINDOW_CLOSED:
         break
-    elif event == "ChooseFiles":
-        file_paths = values["files"].split(";")
+
+    if event == "File Compressor":
+        filepaths = values["files"].split(";")
         folder = values["folder"]
-        make_archive(file_paths, folder)
-        if file_paths:
-            filesInput.update(file_paths)
-    elif event == "File Compressor":
-        # Add your compression logic here
-        print("Selected files:", values[0])
-        print("Destination folder:", values[1])
+
+        if filepaths and folder:
+            make_archive(filepaths, folder)
+
+            window["output"].update("Compression Completed!")
+        else:
+            window["output"].update("Please select both files and destination folder.")
 
 window.close()
